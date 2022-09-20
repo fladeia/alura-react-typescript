db6e2ecf497f9c9184f667fe6ee12961195e96ee
fladeia <ladeia.dev.br@gmail.com>
2022-09-20 15:27:33 -0300

    1 file modified, 1 added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import Form from './components/Form';
+import List from './components/List';
 import './style.css';
 
 export default function App() {
   return (
     <div>
       <Form />
+      <List />
     </div>
   );
 }
